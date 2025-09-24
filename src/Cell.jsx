ac050ab759eefe57e731ff074882f16d21492bca
Muthu kumar <muthukumar24081999@gmail.com>
2025-09-24 15:23:09 +0530

    1st commit

---
+++ src/Cell.jsx
@@ -0,0 +1,13 @@
+const Cell = ({ filled, onClick, isDisabled, label }) => {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
+};
+
+export default Cell;
